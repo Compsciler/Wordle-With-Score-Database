44b74e73e3e867594591d5f3b7ccf5ebe5d3b97d
Roger Wang <rogerwang2520@gmail.com>
2022-05-03 18:04:00 -0500

    Restructure project, prepare app for single Heroku app

---
+++ src/server/index.js
@@ -0,0 +1,41 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+const express = require('express')
+const app = express()
+app.use(express.json())
+const cors = require('cors')
+app.use(cors())
+
+const url = process.env.MONGODB_URI
+
+mongoose.connect(url)
+const scoreSchema = new mongoose.Schema({
+    solutionIndex: Number,
+    solution: String,
+    guesses: [String],
+    lost: Boolean,
+    isHardMode: Boolean,
+    emojiGrid: String
+})
+
+const Score = mongoose.model('Score', scoreSchema)
+
+app.post('/api/scores', (req, res) => {
+    const score = new Score(req.body)
+
+    score.save().then(savedScore => {
+        res.json(savedScore)
+    })
+})
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+app.use(express.static(path.join(__dirname, "../build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
---
+++ src/server/services/scores.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/api/scores'
+
+/*
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+*/
+
+const create = (newObject) => {
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
+}
+
+/*
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+*/
+
+const scoreService = {create}
+export default scoreService
