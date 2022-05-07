09b642eefbad6438d7232912fde72a54b6bafaf4
Roger Wang <rogerwang2520@gmail.com>
2022-05-07 13:50:28 -0500

    Add production NODE_ENV if block in index.js

--- src/server/index.js
+++ src/server/index.js
@@ -35,8 +35,10 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
 
-app.use(express.static(path.join(__dirname, '../../build')))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, '../../build')))
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../build', 'index.html'))
-})
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'))
+  })
+}
