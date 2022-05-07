631e757574db867cfb86bc8cccb9dd16cde3e152
Roger Wang <rogerwang2520@gmail.com>
2022-05-07 02:01:31 -0500

    Try fixing unexpected token '<' 2

--- src/server/index.js
+++ src/server/index.js
@@ -38,7 +38,7 @@ app.listen(PORT, () => {
 
 app.use(express.static(path.join(__dirname, '../../build')))
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../../build', 'index.html'))
 })
 
