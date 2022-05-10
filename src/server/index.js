3a4fba21f44e7ee1952f85559299d5e0e4352fce
Roger Wang <rogerwang2520@gmail.com>
2022-05-09 21:42:25 -0500

    Try fixing React routing for Heroku 2

--- src/server/index.js
+++ src/server/index.js
@@ -41,4 +41,8 @@ if (process.env.NODE_ENV === "production") {
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../build', 'index.html'))
   })
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+  })
 }
