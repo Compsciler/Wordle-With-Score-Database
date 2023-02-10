829debbedefdcf4580b1cb28785376e6d258363d
Roger Wang <rogerwang2520@gmail.com>
2023-02-09 19:22:42 -0600

    Update links for Netlify frontend hosting with Heroku backend 2

--- src/services/scores.js
+++ src/services/scores.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://wordlebackend.herokuapp.com/api/scores'
+const baseUrl = 'https://wordlebackend.herokuapp.com/wordletemplate/api/scores'
 
 /*
 const getAll = () => {
